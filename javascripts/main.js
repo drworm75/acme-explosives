0fa59477e7edacc14da08bb0d8617dccb0078a07
drworm75 <dwgames@gmail.com>
2017-04-18 18:20:37 -0500

    Project working

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,23 +1,61 @@
 $(document).ready(function(){
 	var allExplosive = [];
 
-    function writeDOM(){
-        var domString = "";
+    function createDropdownList(){
         for(var i=0; i<allExplosive.length; i++) {
-    		if (allExplosive[i].name !== undefined) {
-            domString += `<h1>${allExplosive[i].name}</h1>`;
-	        }
-        }
-	    $.each (allExplosive, function (index,value) {
-	    	$.each (value, function (index2, value2){
-	    		if (value2.name !== undefined) {
-	    			// console.log(value2.name, index);
-	    		domString += `<h1>${value2.name}</h1>`;
-		    	}
-	    	});
-	    });
-        $("#promises").append(domString);
+        	if (allExplosive[i].category === undefined && allExplosive[i].name !== undefined ) {
+	    	$('.dropdown-menu').append(`<li value="${allExplosive[i].id}"><p>${allExplosive[i].name}</p></li>`);
+		    }
+		}
 	}
+    		
+	    function writeDOM(cat ,catName){
+		var domString = "";
+		domString += `<div class="row">`
+		domString += `<div class="col-md-12 categories">`
+		domString += `<div class="table-responsive">`
+		domString += `<table class="table">`
+		domString += `<th>${catName}</th>`
+		domString += `</table></div></div></div>`
+		domString += `<div class="row">`
+
+        for(var i=0; i<allExplosive.length; i++) {
+    		if (allExplosive[i].name !== undefined && allExplosive[i].category === cat) {
+			domString += `<div class="col-md-4">`
+			domString += `<div class="table-responsive">`
+			domString += `<table class="table products">`
+    		domString += `<tr>`
+			domString += `<th><h2> ${allExplosive[i].name}</h2></th>`
+			domString += `</tr>`
+		    $.each (allExplosive, function (index,value) {
+		    	$.each (value, function (index2, value2){
+		    		if (value2.name !== undefined && value2.type === allExplosive[i].id) {
+					domString += `<tr>`
+					domString += `<td><h3>${value2.name}</h3></td>`
+					domString += `</tr>`
+			    	}
+		    	});
+		    });
+        }
+	        
+		domString += `</table>`
+		domString += `</div></div>`
+        }
+        
+		domString += `</div>`
+        $("#promises").html(domString);
+	}
+
+	$('.dropdown-menu').on('click', 'li', function(e) {
+	var btnVal = ($(this).val());
+	var btnCat = ($(this).text());
+	console.log(btnCat);
+	writeDOM(btnVal, btnCat);
+
+	});
+
+
+
 
 	var categoriesJSON = function() {
 		return new Promise(function(resolve, reject) {
@@ -53,13 +91,13 @@ $(document).ready(function(){
 
     Promise.all([categoriesJSON(), typesJSON(), productsJSON()])
         .then(function(info){
-            console.log("info", info);
             info.forEach(function(ajaxCalls){
                 ajaxCalls.forEach(function(bangs){
                     allExplosive.push(bangs);
                 });
             });
-            writeDOM();
+            console.log(allExplosive);
+            createDropdownList();
         });	
 });
 
